7c1f8b6116421f827e9702f2fb7d4590730d5e47
Benjamin V Dev <113056824+Benjamin-V-Dev@users.noreply.github.com>
2026-01-12 08:22:55 +0100

    Remove Youtube

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -64,22 +64,6 @@ export const Footer = () => {
                             </svg>
                         </a>
                     </li>
-                    <li>
-                        <a
-                            href='https://www.youtube.com/@ProVision-GV'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                            className='flex items-center justify-center w-12 h-12 rounded-full bg-gray-900 hover:bg-red-600 transition-all duration-300 transform hover:scale-110 group'
-                            aria-label='YouTube'>
-                            <svg
-                                className='w-6 h-6 text-gray-300 group-hover:text-white transition-colors'
-                                fill='currentColor'
-                                viewBox='0 0 24 24'
-                                xmlns='http://www.w3.org/2000/svg'>
-                                <path d='M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z' />
-                            </svg>
-                        </a>
-                    </li>
                 </ul>
 
                 {/* Copyright */}
